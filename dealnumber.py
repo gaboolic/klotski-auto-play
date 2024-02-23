1a84c3468213d348a7e72fd89ab909e05c052d95
gaboolic <23441099@qq.com>
2024-02-23 20:58:33 +0800

    优化路径

--- dealnumber.py
+++ dealnumber.py
@@ -1,17 +1,35 @@
 import cv2
 import numpy as np
+
 # 读取图像
-image = cv2.imread('game.jpg')
+# image = cv2.imread('game.jpg')
+image = cv2.imread('gameimg/99/99.jpg')
 
-one_w = 710
-one_h = 344
+width = 2400
+height = 1080
+
+# 107 21
+# 13 2
+# 15 2
+# 132 21
+# 左边120 右边147 下面黑条21
+# 小图236 113
+# 小图236 114
 start_x = 120
-start_y = 23
+start_y = 21
+end_x = 147
+end_y = 21
 
-width = one_w * 3  # 区域宽度
-height = one_h * 3  # 区域高度
+roi_width = width - start_x - end_x
+roi_height = height - start_y - end_y
 
-image = image[start_y:start_y + height, start_x:start_x + width]
+split_count = 9
+# one_w = 710
+# one_h = 344
+one_w = (roi_width) // split_count
+one_h = (roi_height) // split_count
+
+image = image[start_y:start_y + roi_height, start_x:start_x + roi_width]
 ori_image = image
 cv2.imwrite('temp/roi.jpg', image)
 
@@ -32,9 +50,25 @@ kernel = np.ones((1, 1), np.uint8)
 image = cv2.morphologyEx(image, cv2.MORPH_ERODE, kernel, iterations=2)
 cv2.imwrite('temp/closing.jpg', image)
 
+little_images = []
+for i in range(split_count):
+    for j in range(split_count):
+        # 计算当前小区域的起始坐标
+        start_x = j * (one_w )
+        start_y = i * (one_h )
+
+        # 提取中心数字图像
+        number_image = image[start_y: start_y + one_h, start_x: start_x + one_w]
+
+        # 存储提取的中心数字图像
+        little_images.append(number_image)
+
+for idx, num_img in enumerate(little_images):
+    cv2.imwrite(f'temp/little_{idx}.jpg', num_img)  # 保存图像
+
 number_images = []
-for i in range(3):
-    for j in range(3):
+for i in range(split_count):
+    for j in range(split_count):
         # 计算当前小区域的起始坐标
         start_x = j * one_w
         start_y = i * one_h
@@ -46,11 +80,10 @@ for i in range(3):
         # 提取当前小区域的图像
         half_size = 28  # 因为我们想要20像素，所以取一半的大小
         # 提取中心数字图像
-        number_image = image[center_y: center_y + half_size, center_x - 14: center_x + 14]
+        number_image = image[center_y: center_y + half_size, center_x - 15: center_x + 25]
 
         # 存储提取的中心数字图像
         number_images.append(number_image)
 
 for idx, num_img in enumerate(number_images):
     cv2.imwrite(f'temp/number_{idx}.jpg', num_img)  # 保存图像
-
