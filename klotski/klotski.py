e8a52277fe4a6b586a27282dbad89e6120939a2a
gaboolic <23441099@qq.com>
2024-02-24 21:25:01 +0800

    klotski

--- klotski/klotski.py
+++ klotski/klotski.py
@@ -1,12 +1,16 @@
+import math
 from queue import Queue
 
 # 定义初始状态和目标状态
-# start_state = [1, 4, 8, 0, 7, 2, 5, 6, 3]
-goal_state = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+# start_state = [0, 1, 2, 4, 13, 5, 6, 7, 8, 18, 9, 11, 3, 22, 25, 33, 16, 17, 10, 20, 28, 12, 24, 14, 15, 35, 34, 39, 27, 21, 32, 30, 51, 23, 26, 43, 19, 29, 47, 46, 31, 41, 50, 59, 44, 36, 45, 37, 38, 68, 61, 42, 77, 53, 54, 55, 74, 56, 49, 40, 66, 58, 62, 63, 64, 75, 48, 67, 78, 57, 70, 79, 72, 73, 99, 65, 76, 69, 60, 52, 71]
+goal_state = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 99]
+
 
 def print_puzzle(state):
-    for i in range(0, 9, 3):
-        print(state[i:i + 3])
+    total = len(state)
+    length = int(math.sqrt(total))
+    for i in range(0, total, length):
+        print(state[i:i + length])
 
 def move(state, from_i, to_i):
     state_copy = state.copy()
@@ -26,10 +30,13 @@ def bfs(start_state, goal_state):
 
         zero_index = current_state.index(99)
 
-        for move_to in [1, -1, 3, -3]:
+        total = len(start_state)
+        length = int(math.sqrt(total))
+
+        for move_to in [1, -1, length, -length]:
             new_index = zero_index + move_to
 
-            if 0 <= new_index < 9 and (zero_index % 3 != 0 or move_to != -1) and (zero_index % 3 != 2 or move_to != 1):
+            if 0 <= new_index < total and (zero_index % length != 0 or move_to != -1) and (zero_index % length != (length-1) or move_to != 1):
                 new_state = move(current_state, zero_index, new_index)
 
                 if tuple(new_state) not in visited:
--- test.py
+++ test.py
@@ -16,13 +16,16 @@ import uiautomator2 as u2
 # d.click(0.178, 0.49)
 
 # 读取图像
-# image = cv2.imread('errgameimg/number_2880c950-3305-4b17-afd1-5b09acbf620f.jpg')
-image = cv2.imread('gameimg/99/99.jpg')
-numbers = img2arr.img2arr(image,9)
-print(numbers)
+# image = cv2.imread('errgameimg/game2.jpg')
+# image = cv2.imread('gameimg/99/99.jpg')
+# numbers = img2arr.img2arr(image,3)
+# print(numbers)
+
+# numbers = [0, 1, 2, 3, 5, 6, 20, 78, 99]
+numbers = [0, 1, 2, 4, 13, 5, 6, 7, 8, 18, 9, 11, 3, 22, 25, 33, 16, 17, 10, 20, 28, 12, 24, 14, 15, 35, 34, 39, 27, 21, 32, 30, 51, 23, 26, 43, 19, 29, 47, 46, 31, 41, 50, 59, 44, 36, 45, 37, 38, 68, 61, 42, 77, 53, 54, 55, 74, 56, 49, 40, 66, 58, 62, 63, 64, 75, 48, 67, 78, 57, 70, 79, 72, 73, 99, 65, 76, 69, 60, 52, 71]
 
 # numbers = sorted(numbers)
-# print(numbers)
+print(sorted(numbers))
 
 steps = klotski.get_path(numbers)
 print(steps)
