6a1c736d47a489503ca1dc2c85873494d8312935
gaboolic <23441099@qq.com>
2024-02-25 23:26:27 +0800

    klotski99

--- klotski/klotski99.py
+++ klotski/klotski99.py
@@ -1,31 +1,36 @@
 import time
 from queue import PriorityQueue
 
+blank_num = 0
+
+
 # 计算曼哈顿距离
 def manhattan_distance(state, goal, target_count):
     distance = 0
     for i in range(len(state)):
         for j in range(len(state[i])):
-            if state[i][j] != 0:
+            if state[i][j] != blank_num:
                 row, col = divmod(state[i][j] - 1, 9)
                 distance += abs(i - row) + abs(j - col)
     return distance
+
 
 def manhattan_distance_bottom(state, goal):
     distance = 0
     for i in range(len(state)):
         for j in range(len(state[i])):
-            if state[i][j] != 0:
+            if state[i][j] != blank_num:
                 row, col = divmod(state[i][j] - 1, 9)
                 row -= 7
                 distance += abs(i - row) + abs(j - col)
     return distance
 
+
 # A*算法
 def a_star(start, goal, target_count):
     moves = [(0, -1), (0, 1), (-1, 0), (1, 0)]  # 左、右、上、下
     frontier = PriorityQueue()
-    frontier.put((0, start, []))
+    frontier.put((blank_num, start, []))
     visited = set()
 
     while not frontier.empty():
@@ -39,26 +44,29 @@ def a_star(start, goal, target_count):
 
         visited.add(tuple(map(tuple, current_state)))
 
-        zero_row, zero_col = next((i, j) for i, row in enumerate(current_state) for j, val in enumerate(row) if val == 0)
+        zero_row, zero_col = next(
+            (i, j) for i, row in enumerate(current_state) for j, val in enumerate(row) if val == blank_num)
 
         for move in moves:
             new_row, new_col = zero_row + move[0], zero_col + move[1]
 
             if 0 <= new_row < len(start) and 0 <= new_col < 9:
                 new_state = [row.copy() for row in current_state]
-                new_state[zero_row][zero_col], new_state[new_row][new_col] = new_state[new_row][new_col], new_state[zero_row][zero_col]
+                new_state[zero_row][zero_col], new_state[new_row][new_col] = new_state[new_row][new_col], \
+                    new_state[zero_row][zero_col]
 
                 if tuple(map(tuple, new_state)) not in visited:
-                    distance = manhattan_distance(new_state,goal,target_count)
+                    distance = manhattan_distance(new_state, goal, target_count)
                     cost = len(path) + 1 + distance
                     frontier.put((cost, new_state, path + [(new_row, new_col)]))
 
     return None
+
 
 def a_star_bottom(start, goal, target_count):
     moves = [(0, -1), (0, 1), (-1, 0), (1, 0)]  # 左、右、上、下
     frontier = PriorityQueue()
-    frontier.put((0, start, []))
+    frontier.put((blank_num, start, []))
     visited = set()
 
     while not frontier.empty():
@@ -72,25 +80,28 @@ def a_star_bottom(start, goal, target_count):
 
         visited.add(tuple(map(tuple, current_state)))
 
-        zero_row, zero_col = next((i, j) for i, row in enumerate(current_state) for j, val in enumerate(row) if val == 0)
+        zero_row, zero_col = next(
+            (i, j) for i, row in enumerate(current_state) for j, val in enumerate(row) if val == blank_num)
 
         for move in moves:
             new_row, new_col = zero_row + move[0], zero_col + move[1]
 
             if 0 <= new_row < len(start) and 0 <= new_col < 9:
                 new_state = [row.copy() for row in current_state]
-                new_state[zero_row][zero_col], new_state[new_row][new_col] = new_state[new_row][new_col], new_state[zero_row][zero_col]
+                new_state[zero_row][zero_col], new_state[new_row][new_col] = new_state[new_row][new_col], \
+                    new_state[zero_row][zero_col]
 
                 if tuple(map(tuple, new_state)) not in visited:
                     distance = 0
                     if len(start) == 9:
-                        distance = manhattan_distance(new_state,goal,target_count)
+                        distance = manhattan_distance(new_state, goal, target_count)
                     else:
-                        distance = manhattan_distance_bottom(new_state,goal)
+                        distance = manhattan_distance_bottom(new_state, goal)
                     cost = len(path) + 1 + distance
                     frontier.put((cost, new_state, path + [(new_row, new_col)]))
 
     return None
+
 
 # 打印棋盘状态
 def print_board(state):
@@ -98,76 +109,87 @@ def print_board(state):
         print(row)
     print()
 
-start_time = time.time()
-start = [[1, 2, 3, 5, 14, 6, 7, 8, 9], [19, 10, 12, 4, 23, 26, 34, 17, 18], [11, 21, 29, 13, 25, 15, 16, 36, 35], [40, 28, 22, 33, 31, 52, 24, 27, 44], [20, 30, 48, 47, 32, 42, 51, 60, 45], [37, 46, 38, 39, 69, 62, 43, 78, 54], [55, 56, 75, 57, 50, 41, 67, 59, 63], [64, 65, 76, 49, 68, 79, 58, 71, 80], [73, 74, 0, 66, 77, 70, 61, 53, 72]]
 
-# 目标状态
-goal = [[1, 2, 3, 4, 5, 6, 7, 8, 9], [10, 11, 12, 13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24, 25, 26, 27], [28, 29, 30, 31, 32, 33, 34, 35, 36], [37, 38, 39, 40, 41, 42, 43, 44, 45], [46, 47, 48, 49, 50, 51, 52, 53, 54], [55, 56, 57, 58, 59, 60, 61, 62, 63], [64, 65, 66, 67, 68, 69, 70, 71, 72], [73, 74, 75, 76, 77, 78, 79, 80, 0]]
+def get_path(start, goal):
+    start_time = time.time()
 
-path, current_state = a_star(start, goal, 2)
-print(path)
-print(current_state)
+    path, current_state = a_star(start, goal, 2)
+    print(path)
+    print(current_state)
 
+    step_indexs = []
+    # 执行A*算法
+    for i in range(63):
+        print("start")
+        print(i + 1)
+        print(start)
+        path, current_state = a_star(start, goal, i + 1)
 
-# 执行A*算法
-for i in range(63):
+        if path:
+            current_state = [row.copy() for row in start]  # 初始化当前状态为初始状态
+            zero_row, zero_col = next(
+                (i, j) for i, row in enumerate(start) for j, val in enumerate(row) if val == blank_num)
+
+            print("初始状态：")
+            print_board(current_state)
+
+            print("移动步骤：")
+            for step, (row, col) in enumerate(path):
+                current_state[row][col], current_state[zero_row][zero_col] = current_state[zero_row][zero_col], \
+                    current_state[row][col]
+                print(f"Step {step + 1}: Move 0 to ({row}, {col})")
+                print(current_state)
+                zero_row, zero_col = row, col
+                step_indexs.append((row, col))
+        start = current_state
+
+    print("已经拼好前n-2层")
+
+    start = start[-2:]
     print("start")
-    print(i+1)
     print(start)
-    path, current_state = a_star(start, goal, i+1)
 
-    if path:
-        current_state = [row.copy() for row in start]  # 初始化当前状态为初始状态
-        zero_row, zero_col = next((i, j) for i, row in enumerate(start) for j, val in enumerate(row) if val == 0)
+    for i in range(18):
+        print("start")
+        print(i + 1)
+        print(start)
+        path, current_state = a_star_bottom(start[-2:], goal[-2:], i + 1)
 
-        print("初始状态：")
-        print_board(current_state)
+        if path:
+            current_state = [row.copy() for row in start]  # 初始化当前状态为初始状态
+            zero_row, zero_col = next(
+                (i, j) for i, row in enumerate(start) for j, val in enumerate(row) if val == blank_num)
 
-        print("移动步骤：")
-        for step, (row, col) in enumerate(path):
-            current_state[row][col], current_state[zero_row][zero_col] = current_state[zero_row][zero_col], \
-            current_state[row][col]
-            print(f"Step {step + 1}: Move 0 to ({row}, {col})")
-            print(current_state)
-            zero_row, zero_col = row, col
+            print("初始状态：")
+            print_board(current_state)
 
-    else:
-        print("无须操作")
+            print("移动步骤：")
+            for step, (row, col) in enumerate(path):
+                current_state[row][col], current_state[zero_row][zero_col] = current_state[zero_row][zero_col], \
+                    current_state[row][col]
+                print(f"Step {step + 1}: Move 0 to ({row}, {col})")
+                print(current_state)
+                zero_row, zero_col = row, col
+                step_indexs.append((row, col))
 
-    start = current_state
-
-print("已经拼好前n-2层")
-
-start = start[-2:]
-print("start")
-print(start)
-
-for i in range(18):
-    print("start")
-    print(i+1)
-    print(start)
-    path, current_state = a_star_bottom(start[-2:], goal[-2:], i+1)
-
-    if path:
-        current_state = [row.copy() for row in start]  # 初始化当前状态为初始状态
-        zero_row, zero_col = next((i, j) for i, row in enumerate(start) for j, val in enumerate(row) if val == 0)
-
-        print("初始状态：")
-        print_board(current_state)
-
-        print("移动步骤：")
-        for step, (row, col) in enumerate(path):
-            current_state[row][col], current_state[zero_row][zero_col] = current_state[zero_row][zero_col], \
-            current_state[row][col]
-            print(f"Step {step + 1}: Move 0 to ({row}, {col})")
-            print(current_state)
-            zero_row, zero_col = row, col
-
-    else:
-        print("无须操作")
-
-    start = current_state
-end_time = time.time()
-print(f"{end_time-start_time}秒")
+        start = current_state
+    end_time = time.time()
+    print(f"{end_time - start_time}秒")
+    return step_indexs
 
 
+# start = [[1, 2, 3, 5, 14, 6, 7, 8, 9], [19, 10, 12, 4, 23, 26, 34, 17, 18], [11, 21, 29, 13, 25, 15, 16, 36, 35],
+#          [40, 28, 22, 33, 31, 52, 24, 27, 44], [20, 30, 48, 47, 32, 42, 51, 60, 45],
+#          [37, 46, 38, 39, 69, 62, 43, 78, 54], [55, 56, 75, 57, 50, 41, 67, 59, 63],
+#          [64, 65, 76, 49, 68, 79, 58, 71, 80], [73, 74, 0, 66, 77, 70, 61, 53, 72]]
+#
+#
+#
+# # 目标状态
+goal = [[1, 2, 3, 4, 5, 6, 7, 8, 9], [10, 11, 12, 13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24, 25, 26, 27],
+        [28, 29, 30, 31, 32, 33, 34, 35, 36], [37, 38, 39, 40, 41, 42, 43, 44, 45],
+        [46, 47, 48, 49, 50, 51, 52, 53, 54], [55, 56, 57, 58, 59, 60, 61, 62, 63],
+        [64, 65, 66, 67, 68, 69, 70, 71, 72], [73, 74, 75, 76, 77, 78, 79, 80, 0]]
+#
+# paths = get_path(start, goal)
+# print(paths)
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import cv2
 import uiautomator2 as u2
 
 from recognition import img2arr
+from klotski import klotski99
 
 # d = u2.connect() # connect to device
 # d = u2.connect_wifi('192.168.1.3') # connect to device
@@ -13,16 +14,24 @@ from recognition import img2arr
 # 读取图像
 # image = cv2.imread('errgameimg/game2.jpg')
 # image = cv2.imread('gameimg/55/55.jpg')
-image = cv2.imread('gameimg/99/99_xiandai_shengdan.jpg')
-numbers = img2arr.img2arr(image,9)
+image = cv2.imread('gameimg/99/99.jpg')
+# numbers = img2arr.img2arr(image,9)
+
+numbers = [0, 1, 2, 4, 13, 5, 6, 7, 8, 18, 9, 11, 3, 22, 25, 33, 16, 17, 10, 20, 28, 12, 24, 14, 15, 35, 34, 39, 27, 21,
+           32, 30, 51, 23, 26, 43, 19, 29, 47, 46, 31, 41, 50, 59, 44, 36, 45, 37, 38, 68, 61, 42, 77, 53, 54, 55, 74,
+           56, 49, 40, 66, 58, 62, 63, 64, 75, 48, 67, 78, 57, 70, 79, 72, 73, 99, 65, 76, 69, 60, 52, 71]
+numbers = [-1 if num == 99 else num for num in numbers]
+
+# 将数组中所有元素加1
+numbers = [num + 1 for num in numbers]
 print(numbers)
 
 sorted_numbers = sorted(numbers)
 print(sorted_numbers)
 
 split_count = 9
-given_array = list(range(split_count * split_count - 1))
-given_array.append(99)
+given_array = list(range(1, split_count * split_count))
+given_array.append(0)
 
 # 检查排序后的数组是否等于给定数组
 if sorted_numbers != given_array:
@@ -30,16 +39,16 @@ if sorted_numbers != given_array:
 else:
     print("识别的图像正确")
 
-
-# 将数组中所有元素加1
-numbers = [num + 1 for num in numbers]
-
 # 将一维数组转换为二维数组
-numbers_2d = [numbers[i:i+9] for i in range(0, len(numbers), 9)]
+numbers_2d = [numbers[i:i + 9] for i in range(0, len(numbers), 9)]
 print(numbers_2d)
 
 # 将数组中所有元素加1
-sorted_numbers = [num + 1 for num in sorted_numbers]
+# sorted_numbers = [num + 1 for num in sorted_numbers]
 # 将一维数组转换为二维数组
-sorted_numbers_2d = [sorted_numbers[i:i+9] for i in range(0, len(sorted_numbers), 9)]
-print(sorted_numbers_2d)
+given_array = [given_array[i:i + 9] for i in range(0, len(given_array), 9)]
+print(given_array)
+
+paths = klotski99.get_path(numbers_2d, given_array)
+print(len(paths))
+print(paths)
