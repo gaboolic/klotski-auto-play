5f822096b2a863018368dc9a8a0254b1c77302f7
gaboolic <23441099@qq.com>
2024-03-01 15:52:23 +0800

    sllm 100

---
+++ recognition/img2arr_sllm.py
@@ -0,0 +1,86 @@
+import time
+
+import cv2
+import numpy as np
+from tensorflow import keras
+
+# 加载模型
+lenet5_20_model = keras.models.load_model('recognition/lenet5_20.keras')
+
+
+def one_img(image):
+    # 计算平均像素值
+    average_pixel_value = cv2.mean(image)[0]
+
+    # 阈值化检查图像是否全黑或全白
+    _, binary_image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+    unique_values = cv2.countNonZero(binary_image)
+
+    if average_pixel_value < 10:
+        return 0
+    elif average_pixel_value > 245:
+        return 0
+
+    resize_img = cv2.resize(image, (28, 28))  # 调整大小为28x28
+    image = resize_img / 255.0  # 归一化
+    # 添加批次维度
+    image = np.expand_dims(image, axis=0)
+
+    # 进行预测
+    predictions = lenet5_20_model.predict(image)
+    # 打印预测结果
+    predicted_label = np.argmax(predictions[0])
+    # cv2.imwrite(f'temp/predict_{predicted_label}_{time.time()}.jpg', resize_img)  # 保存图像
+    return predicted_label
+
+
+def img2arr(image, split_count):
+    # 获取图像宽度和高度
+    height, width, _ = image.shape
+
+    width = 1080
+    height = 2400
+
+    # 100 100
+    start_x = 19
+    start_y = 666
+
+    one_w = 1062 // 9 - 20
+    one_h = 1062 // 9 - 20
+
+    image = image[start_y:start_y + 120 * 9 - 20, start_x:start_x + 120 * 9 - 20]
+    cv2.imwrite('temp/roi.jpg', image)
+
+    # 将图像转换为灰度图像
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # 对图像进行二值化处理
+    ret, image = cv2.threshold(image, 150, 255, cv2.THRESH_BINARY)
+    # ret, image = cv2.threshold(image, 254, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    # 对图像进行形态学操作（可选）
+    kernel = np.ones((1, 1), np.uint8)
+    image = cv2.morphologyEx(image, cv2.MORPH_ERODE, kernel, iterations=2)
+
+    number_images = []
+    for i in range(split_count):
+        for j in range(split_count):
+            # 计算当前小区域的起始坐标
+            start_x = j * (one_w + 20)
+            start_y = i * (one_h + 20)
+            # 98
+            # 54
+            half = 22
+            # 提取中心数字图像
+            number_image = image[start_y+22: start_y + one_h-22, start_x+22: start_x + one_w-22]
+            cv2.imwrite(f'temp/number__{i}_{j}.jpg', number_image)  # 保存图像
+
+            # 存储提取的中心数字图像
+            number_images.append(number_image)
+
+    numbers = []
+    for idx, num_img in enumerate(number_images):
+        number = one_img(num_img)
+        numbers.append(number)
+        cv2.imwrite(f'temp_number/number_{number}_{time.time()}.jpg', num_img)  # 保存图像
+    return numbers
---
+++ recognition/train_lenet100.py
@@ -0,0 +1,53 @@
+import os
+import tensorflow as tf
+from tensorflow import keras
+import cv2
+import numpy as np
+
+def translate_image(image, tx_range, ty_range):
+    tx = np.random.randint(-tx_range, tx_range + 1)
+    ty = np.random.randint(-ty_range, ty_range + 1)
+
+    rows, cols = image.shape
+    M = np.float32([[1, 0, tx], [0, 1, ty]])
+    translated_image = cv2.warpAffine(image, M, (cols, rows))
+
+    return translated_image
+
+train_images = []
+train_labels = []
+for i in range(100):
+    # 读取图像并进行预处理
+    path = f'../number_template/{i}/'
+    print(path)
+    # 读取path下所有图片
+    files = os.listdir(path)
+    # 筛选出所有以'.jpg'或'.png'结尾的文件
+    image_files = [f for f in files if f.endswith('.jpg') or f.endswith('.png')]
+
+    # 打印所有图片文件名
+    for image_file in image_files:
+        print(image_file)
+        image = cv2.imread(path + image_file, cv2.IMREAD_GRAYSCALE)  # 以灰度图像方式读取
+        image = cv2.resize(image, (28, 28))  # 调整大小为28x28
+
+        # 随机平移图像
+        tx_range = 1  # x轴平移范围
+        ty_range = 1  # y轴平移范围
+        image = translate_image(image, tx_range, ty_range)
+
+        image = image / 255.0  # 归一化
+        train_images.append(image)
+        train_labels.append(i)
+
+train_images = np.array(train_images)
+train_labels = np.array(train_labels)
+
+# 加载模型
+model = keras.models.load_model('lenet5_100.keras')
+
+# 训练模型
+model.fit(train_images, train_labels, epochs=60)
+
+# 保存模型
+model.save('lenet5_100.keras')
